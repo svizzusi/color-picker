eb71e85288eb0102f550b3d81a878a7735c9f5e8
Stephen Vizzusi <svizzusi13@gmail.com>
2023-05-07 12:17:05 -0700

    Added a Toast notification

--- js/main.js
+++ js/main.js
@@ -1,17 +1,18 @@
-const colors = ["crimson", "dodgerblue", "purple", "yellow", "orange", "lightgreen"]
+const basicColors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
 
 const hexColors = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"]
 
 let randomButton = document.querySelector('.rd-btn')
 let button = document.querySelector(".btn")
-let color = document.querySelector(".color")
-let bgRandom = document.querySelector(".bg-random")
+let colorText = document.querySelector(".color-text")
+let bgText = document.querySelector(".bg-text")
+let toastAlert = document.querySelector("#toast-notification")
 
 button.addEventListener("click", function() {
     let randomNumber = getRandomNumber();
-    console.log(randomNumber)
-    let body = document.body.style.backgroundColor = colors[randomNumber];
-    color.innerText = " " + colors[randomNumber]
+    let body = document.body.style.backgroundColor = basicColors[randomNumber];
+    colorText.innerText = " " + basicColors[randomNumber]
+    toastAlert.innerText = basicColors[randomNumber] + " Copied to clipboard"
 });
 
 randomButton.addEventListener('click', function() {
@@ -20,41 +21,49 @@ randomButton.addEventListener('click', function() {
         hexColor += hexColors[getRandomHexColor()];
     }
     let body = document.body.style.backgroundColor = hexColor;
-    color.innerText = " " + hexColor
+    colorText.innerText = " " + hexColor
+    toastAlert.innerText = hexColor + " Copied to clipboard"
 })
 
 function getRandomNumber() {
-    return Math.floor(Math.random() * colors.length)
+    return Math.floor(Math.random() * basicColors.length)
 }
 
 function getRandomHexColor() {
     return Math.floor(Math.random() * hexColors.length)
 }
 
-bgRandom.addEventListener("click", function() {
-    let colorValue = color.innerText;
-  
-    // check if the Clipboard API is supported
-    if (navigator.clipboard) {
-      navigator.clipboard.writeText(colorValue)
-        .then(() => {
-          alert(colorValue + " " + "Copied to Clipboard");
-        })
-        .catch((error) => {
-          console.error("Failed to copy text: ", error);
-        });
-    } else {
-      // Clipboard API not available, try using a fallback method
-      try {
-        const inputElement = document.createElement("textarea");
-        inputElement.value = colorValue;
-        document.body.appendChild(inputElement);
-        inputElement.select();
-        document.execCommand("copy");
-        document.body.removeChild(inputElement);
-        alert(colorValue + " " + "Copied to Clipboard");
-      } catch (error) {
-        console.error("Fallback method failed: ", error);
-      }
+function toastNotification() {
+  toastAlert.style.visibility = "visible";
+  setTimeout(function(){
+    toastAlert.style.visibility = "hidden";
+  }, 2000);
+}
+
+bgText.addEventListener("click", function() {
+  let colorValue = colorText.innerText;
+
+  // check if the Clipboard API is supported
+  if (navigator.clipboard) {
+    navigator.clipboard.writeText(colorValue)
+      .then(() => {
+        toastNotification()
+      })
+      .catch((error) => {
+        console.error("Failed to copy text: ", error);
+      });
+  } else {
+    // Clipboard API not available, try using a fallback method
+    try {
+      const inputElement = document.createElement("textarea");
+      inputElement.value = colorValue;
+      document.body.appendChild(inputElement);
+      inputElement.select();
+      document.execCommand("copy");
+      document.body.removeChild(inputElement);
+      toastNotification()
+    } catch (error) {
+      console.error("Fallback method failed: ", error);
     }
-  });
+  }
+});
